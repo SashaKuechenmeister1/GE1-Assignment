1bd7293718330a136219f0314202681cafa07fc9
c18404082 <c18404082@mytudublin.ie>
2021-11-17 13:05:02 +0000

    added some scripts

---
+++ Procedural Island Assignment/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+    
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public void GenerateMap() {
+        float [,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        
+    }
+}
